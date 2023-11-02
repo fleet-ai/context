840087351ee82a1fcae55596ab2a73bd89f740b1
Andrew Zhou <andrew@heykona.com>
2023-11-02 00:52:10 -0700

    Update config files

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="context",
     version="0.1",
+    description="Access up-to-date information about the most important Python libraries in the world.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Fleet AI",
+    author_email="team@usefleet.ai",
+    url="https://github.com/fleet-ai/context/tree/main",
     packages=find_packages(),
     install_requires=["openai", "rich>=13.0.0", "tiktoken>=0.1.1"],
     entry_points={
@@ -10,4 +16,10 @@ setup(
             "context = cli:main",
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
